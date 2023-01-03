6ea37ee1f2d8b32d97c48e33e244ace6d0727c8f
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-03 23:00:09 +0530

    break lsp

--- LiskovSubstitutionPrinciple/lsp.go
+++ LiskovSubstitutionPrinciple/lsp.go
@@ -28,6 +28,29 @@ func (r *Rectangle) SetHeight(height int) {
 	r.height = height
 }
 
+
+type Square struct {
+	Rectangle
+}
+
+// Enforce the idea of width being equal to height
+func NewSquare(size int) *Square {
+	sq := Square{}
+	sq.width = size
+	sq.height = size
+	return &sq
+}
+
+func (s * Square) SetWidth(width int) {
+	s.width = width
+	s.height = width
+}
+
+func (s * Square) SetHeight(height int) {
+	s.height = height
+	s.width = height
+}
+
 func UseIt(sized Sized) {
 	width := sized.GetWidth()
 	sized.SetHeight(10)
@@ -39,4 +62,7 @@ func UseIt(sized Sized) {
 func main() {
 	rc := &Rectangle{10, 20}
 	UseIt(rc)
+
+	sq := NewSquare(6)
+	UseIt(sq)
 }	
