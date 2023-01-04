ea65124eb99b27f671261e4277d44843bd2b58c6
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-04 22:37:03 +0530

    define Print, Fax and Scan method on MultiFunctionPrinter

--- InterfaceSegregationPrinciple/isp.go
+++ InterfaceSegregationPrinciple/isp.go
@@ -10,6 +10,22 @@ type Machine interface {
 	Scan(d Document)
 }
 
+type MultiFunctionPrinter struct {
+
+}
+
+func (m MultiFunctionPrinter) Print(d Document) {
+
+}
+
+func (m MultiFunctionPrinter) Fax(d Document) {
+
+}
+
+func (m MultiFunctionPrinter) Scan(d Document) {
+	
+}
+
 func main() {
 
 }
