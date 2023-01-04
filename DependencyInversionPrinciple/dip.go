a7c0df3029903c81d702a7f3722f477189dc6c01
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-04 23:03:54 +0530

    add description

---
+++ DependencyInversionPrinciple/dip.go
@@ -0,0 +1,11 @@
+// Dependency Inversion Principle
+// The principle states
+
+// 1) High-level modules should not import anything from low-level modules. Both should depend on abstractions (e.g., interfaces).
+// 2) Abstractions should not depend on details. Details (concrete implementations) should depend on abstractions.
+
+package main
+
+func main() {
+
+}
