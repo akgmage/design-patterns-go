4689048f3b33870fd437ae24ba185c66a550721c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-05 23:15:20 +0530

    add description

--- DependencyInversionPrinciple/dip.go
+++ DependencyInversionPrinciple/dip.go
@@ -4,6 +4,7 @@
 // 1) High-level modules should not import anything from low-level modules. Both should depend on abstractions (e.g., interfaces).
 // 2) Abstractions should not depend on details. Details (concrete implementations) should depend on abstractions.
 
+// By abstractions we typically mean interfaces in go
 package main
 
 import "fmt"
