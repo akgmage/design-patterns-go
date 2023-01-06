654690d3b8a0254334db881d50a394c69b9c347e
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-06 22:43:32 +0530

    add description for builder

--- BUILDER/builder.go
+++ BUILDER/builder.go
@@ -3,3 +3,11 @@
 // Having a factory function with 15 arguments is not productive
 // Instead, opt for piecewise (piece-by-piece) construction
 // BUilder provides an API for constructing an object step by step
+
+// Builder : When piecewise object construction is complicated, provide an API for doing it in brief and clear manner
+
+package main
+
+func main () {
+	
+}
