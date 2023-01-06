1c984ebfd4747356ad8bf241a776c1ed7fcd606b
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-06 23:05:46 +0530

    print html

--- BUILDER/builder.go
+++ BUILDER/builder.go
@@ -29,6 +29,17 @@ func (e *HtmlElement) string(indent int) string {
 	sb := strings.Builder{}
 	i := strings.Repeat(" ", indentSize * indent)
 	sb.WriteString(fmt.Sprintf("%s%s\n", i, e.name))
+	if len(e.text) > 0 {
+		sb.WriteString(strings.Repeat(" ", indentSize * (indent + 1)))
+		sb.WriteString(e.text)
+		sb.WriteString("\n")
+	}
+
+	for _, el := range e.elements {
+		sb.WriteString(el.string(indent + 1))
+	}
+	sb.WriteString(fmt.Sprintf("%s%s\n", i, e.name))
+	return sb.String()
 }
 
 
